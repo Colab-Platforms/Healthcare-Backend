285bac44d142a6dd289eba151836c4e65c27d4dd
Colab-Platforms <colabplatforms@gmail.com>
2025-07-15 17:48:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(cors({
+  origin: ['https://www.aayushwellness.com'], // Allow your frontend domain
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
+}));
+
 // **MongoDB Connection**
 const db = async()=>{
   try{
